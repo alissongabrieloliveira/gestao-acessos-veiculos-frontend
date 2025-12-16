70c18050e94b27030a594960d106b2dfcbfd2ac2
Alisson Gabriel <a7.goliveira@gmail.com>
2025-12-15 23:26:36 -0300

    fix: corrige baseURL da api e adiciona log de debug

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
+  // Garante que se a variável estiver vazia, usa o localhost
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3333",
 });
 
